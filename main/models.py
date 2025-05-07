807d49a62b29038fe4de72ba411447d701e07021
root <root@vultr.guest>
2025-05-07 14:39:16 +0000

    Initial commit

---
+++ main/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+
+class Paper(models.Model):
+    title = models.CharField(max_length=200)
+    authors = models.CharField(max_length=200)
+    abstract = models.TextField()
+
+    def __str__(self):
+        return self.title
