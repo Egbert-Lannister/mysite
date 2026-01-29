754a5b6fe887fed475ab1d5553ecc1eaa3952847
Egbert <egbert@example.com>
2026-01-29 13:02:59 +0800

    toc

--- mysite/urls.py
+++ mysite/urls.py
@@ -2,6 +2,7 @@
 URL configuration for mysite project.
 """
 from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import RedirectView
@@ -14,6 +15,10 @@ urlpatterns = [
     path('', RedirectView.as_view(url='/techblog/', permanent=True)),
 ]
 
-# Only include browser reload URLs in development
+# Serve media files in development
 if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns.insert(0, path('__reload__/', include('django_browser_reload.urls')))
+
+# In production, nginx should serve media files
+# But we can also serve them via whitenoise in some cases
